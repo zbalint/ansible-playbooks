d6d2705c4ff8a7ec0fbd136bc37176ab6d572e89
zbalint <balint.zoltan.marton@gmail.com>
2024-03-11 22:17:07 +0100

    wip

--- debian/setup_debian_docker.sh
+++ debian/setup_debian_docker.sh
@@ -1,6 +1,6 @@
 # Add Docker's official GPG key:
-apt-get update
-apt-get install ca-certificates curl gnupg
+apt-get update && \
+apt-get install ca-certificates curl gnupg -y
 install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 chmod a+r /etc/apt/keyrings/docker.gpg
@@ -10,6 +10,6 @@ echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
-apt-get update
-apt install docker.io -y
-apt install docker-compose-plugin -y
+apt-get update && \
+apt-get install docker.io -y && \
+apt-get install docker-compose-plugin -y

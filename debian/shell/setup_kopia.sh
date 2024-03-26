a595e8002e81b28ecf3cdc95844ea0dd33e0493b
zbalint <balint.zoltan.marton@gmail.com>
2024-03-26 21:28:16 +0100

    wip

--- debian/shell/setup_kopia.sh
+++ debian/shell/setup_kopia.sh
@@ -6,10 +6,7 @@ curl -fsSL https://kopia.io/signing-key | gpg --dearmor -o /etc/apt/keyrings/kop
 chmod a+r /etc/apt/keyrings/kopia-keyring.gpg
 
 # Add the repository to Apt sources:
-
-echo \
-  "deb [signed-by=/etc/apt/keyrings/kopia-keyring.gpg] http://packages.kopia.io/apt/ stable main" | \
-  tee /etc/apt/sources.list.d/kopia.list
+echo "deb [signed-by=/etc/apt/keyrings/kopia-keyring.gpg] http://packages.kopia.io/apt/ stable main" | tee /etc/apt/sources.list.d/kopia.list
 
 apt-get update && \
 apt-get install kopia -y

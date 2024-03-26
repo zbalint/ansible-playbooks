88fa2300263efc5ca2cf50e9496855fe7bde735b
zbalint <balint.zoltan.marton@gmail.com>
2024-03-26 21:17:20 +0100

    wip

--- debian/shell/setup_kopia.sh
+++ debian/shell/setup_kopia.sh
@@ -6,10 +6,6 @@ curl -fsSL https://kopia.io/signing-key | gpg --dearmor -o /etc/apt/keyrings/kop
 chmod a+r /etc/apt/keyrings/kopia-keyring.gpg
 
 # Add the repository to Apt sources:
-echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/kopia-keyring.gpg] http://packages.kopia.io/apt/ \
